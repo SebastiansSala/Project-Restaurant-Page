0fcd0227f3b8ae83dc73cdcc449b89c1fe6c4b54
Sebastian Sala <sebastian_orko@hotmail.com>
2023-03-27 14:06:36 -0600

    first commit

---
+++ src/menu-hamburger.js
@@ -0,0 +1,101 @@
+const renderHamburguer = () => {
+    const menu = document.createElement('section');
+    menu.setAttribute("class", "menuwrapper");
+    menu.innerHTML = `
+    <div class="menutitles">
+            <p>MENU</p>
+            <nav class="title-flex">
+              <li><p>Hamburguesas</p></li>
+              <li>Pizzas</li>
+              <li>Tacos</li>
+              <li>Postres</li>
+            </nav>
+          </div>
+          <div class="menu" id="menu">
+            <div class="entradas">
+                <div class="entradas-container">
+                  <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                  <h4 class="foodPrice">$300</h4>
+                  <p class="foodDesc" style="color: #F59223;">Great american foodarstart
+                    arstartsartarstearsntar
+                    arstarosierte
+                  </p>
+                </div>
+                <div class="entradas-container">
+                  <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                  <h4 class="foodPrice">$300</h4>
+                  <p class="foodDesc" style="color: #F59223;">Great american food</p>
+                </div>
+                <div class="entradas-container">
+                  <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                  <h4 class="foodPrice">$300</h4>
+                  <p class="foodDesc" style="color: #F59223;">Great american foodarstart
+                    arstartsartarstearsntar
+                    arstarosierte
+                  </p>
+                </div>
+                <div class="entradas-container">
+                  <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                  <h4 class="foodPrice">$300</h4>
+                  <p class="foodDesc" style="color: #F59223;">Great american food</p>
+                </div>
+            </div>
+    
+            <div class="entradas">
+              <div class="entradas-container">
+                <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                <h4 class="foodPrice">$300</h4>
+                <p class="foodDesc" style="color: #F59223;">Great american food</p>
+              </div>
+              <div class="entradas-container">
+                <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                <h4 class="foodPrice">$300</h4>
+                <p class="foodDesc" style="color: #F59223;">Great american food</p>
+              </div>
+              <div class="entradas-container">
+                <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                <h4 class="foodPrice">$300</h4>
+                <p class="foodDesc" style="color: #F59223;">Great american food</p>
+              </div>
+              <div class="entradas-container">
+                <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                <h4 class="foodPrice">$300</h4>
+                <p class="foodDesc" style="color: #F59223;">Great american foodarstart
+                  arstartsartarstearsntar
+                  arstarosierte
+                </p>
+              </div>
+            </div>
+    
+            <div class="entradas">
+              <div class="entradas-container">
+                <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                <h4 class="foodPrice">$300</h4>
+                <p class="foodDesc" style="color: #F59223;">Great american food</p>
+              </div>
+              <div class="entradas-container">
+                <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                <h4 class="foodPrice">$300</h4>
+                <p class="foodDesc" style="color: #F59223;">Great american foodarstart
+                  arstartsartarstearsntar
+                  arstarosierte
+                </p>
+              </div>
+              <div class="entradas-container">
+                <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                <h4 class="foodPrice">$300</h4>
+                <p class="foodDesc" style="color: #F59223;">Great american foodarstart
+                  arstartsartarstearsntar
+                  arstarosierte
+                </p>
+              </div>
+              <div class="entradas-container">
+                <h3 class="foodtitle" style="color: #003049;">Jamburguer</h3>
+                <h4 class="foodPrice">$300</h4>
+                <p class="foodDesc" style="color: #F59223;">Great american food</p>
+              </div>
+            </div>
+    `  
+};
+
+export {renderHamburguer};
